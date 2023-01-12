36e6bf72f6d6fc1b8db431594aa35a6e7f720717
muhammad.omer.aslam <muhammad.omer.aslam@pwc.com>
2023-01-12 22:55:20 +0100

    adding health check endpoint

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const redisPublisher = redisClient.duplicate();
 // Express route handlers
 
 app.get("/", (req, res) => {
-  res.send("Hi");
+  res.status(200).send('Ok');
 });
 
 app.get("/values/all", async (req, res) => {
